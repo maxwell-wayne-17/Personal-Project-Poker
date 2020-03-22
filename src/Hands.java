990abfa1b5becbffa481f35e00cedbb76e0e8222
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2020-03-22 01:40:00 -0400

    added player stuff

---
+++ src/Hands.java
@@ -0,0 +1,18 @@
+/**
+ * Holds names of all poker hands
+ * @author maxwn
+ *
+ */
+public enum Hands {
+
+		STRAIGHT_FLUSH,
+		FOUR_OF_A_KIND,
+		FULL_HOUSE,
+		FLUSH,
+		STRAIGHT,
+		THREE_OF_A_KIND,
+		TWO_PAIR,
+		PAIR,
+		HIGH_CARD
+	
+}
--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,8 @@
 /**
  * 
  */
+import java.util.ArrayList;
+
 
 /**
  * Class representing a player in the poker game
@@ -8,5 +10,82 @@
  *
  */
 public class Player {
+	
+	/**
+	 * name of player
+	 */
+	private String name;
+	
+	/**
+	 * Total chip amount owned by the player
+	 */
+	private int chipAmount;
+	
+	/**
+	 * Current bet placed by player
+	 */
+	private int betAmount;
+	
+	/**
+	 * folded or not
+	 */
+	private boolean isFolded;
+	
+	/**
+	 * in hand or not
+	 */
+	private boolean inHand;
+	
+	/**
+	 * is dealer or not
+	 */
+	private boolean isDealer;
+	
+	/**
+	 * hand containing two cards
+	 */
+	private ArrayList<Card> hand = new ArrayList<Card>(2);
+	
+	public Player() {
+		
+		this.chipAmount = 0;
+		this.betAmount = 0;
+		this.isFolded = false;
+		this.inHand = true;
+		this.isDealer = false;
+		
+	}
+	
+	public Player(int buyIn) {
+		
+		this.chipAmount = buyIn;
+		this.betAmount = 0;
+		this.isFolded = false;
+		this.inHand = true;
+		this.isDealer = false;
+		
+	}
+	
+	public int placeBet(int bet) {
+		this.chipAmount = chipAmount - bet;
+		this.betAmount = betAmount + bet;
+		return bet;
+	}
+	
+	public void fold() {
+		this.isFolded = true;
+	}
+	
+	public void addChips(int pot) {
+		this.chipAmount = chipAmount + pot;
+	}
+	
+	public Hands findHand(ArrayList<Card> table) {
+		
+		return Hands.FLUSH;
+		
+	}
+	
+
 
 }
